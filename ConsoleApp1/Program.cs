9436383a413d8fe5fe11f93a83b58bb766ee6629
Angel Daniel PASTOR ROJAS <angeldaniel.pastorrojas@gmail.com>
2024-07-05 13:42:37 +0200

    feat ajout de Exo4

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -1,10 +1,14 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
 #region "Exercice1"
-/** Ecrivez un code pour vérifiez si un age on est majeur */
+
 
 //int age = 15;
 
+using System.Runtime.CompilerServices;
+using System.Security.Cryptography;
+
+/** Ecrivez un code pour vérifiez si un age on est majeur */
 static void EstMajeur(int age)
 {
     if (age > 0)
@@ -143,5 +147,105 @@ static void App4()
     }
 }
 
-App4();
+//App4();
+#endregion
+
+#region "Exo 1"
+/** Contraintes:
+ * - Stocker la saisi utilisateur dans une variable de type string.
+ * - Utiliseé une structure if...else afin de retourner la réponse de l'utilisateur
+ */
+static void App5()
+{
+    //Renvoye True si la chaîne de caratère possède 1 caratère
+    static bool isLetter(string str)
+    {
+        return str.Length == 1;
+    }
+
+    bool isEnd = false;
+    while (!isEnd)
+    {
+        string lettre = Console.ReadLine().ToUpper();
+        string voyelle = "AEIOUY";
+        string consonne = "BCDFGHJKLMNPQRSTVWXZ";
+
+        Console.WriteLine("--- La lettre est-elle une voyelle ? ---");
+        Console.Write("\nEntrez une lettre : ");
+
+        if (isLetter(lettre) && voyelle.Contains(lettre))
+        {
+            Console.WriteLine("Cette lettre est une voyelle !\n");
+            isEnd = true;
+        }
+        else if (isLetter(lettre) && consonne.Contains(lettre))
+        {
+            Console.WriteLine("Cette lettre est une consonne !\n");
+            isEnd = true;
+        }
+        else
+        {
+            Console.WriteLine("Votre saisi n'est pas une lettre !\n");
+        }
+    }
+}
+//App5();
+#endregion
+
+#region "Exo 2"
+//Déclaration des variables c = capital, tdi = taux d'interet, dde = durée d'epargne, mdi = montant des intérêts et cf = capital final du type double
+//Affichage des messages pour entrer les valeurs de capital (en euros), taux d'interet et durée epargne
+//et reception des données : c/tdi/dde rentrée par l'utilisateur
+//Calcul du montant des interets : mi = c * tdi * dde grace à la fonction 
+//Affichage message résultats du montant des interet mdi + affichage de dde
+//Affichage de message du capital final avec utilisation de la variable cf
+//14 instructions
+/**
+bool fin = false;
+double c, tdi, dde, mdi, cf;
+while (!fin)
+{
+    ///if (c is null && tdi is null && dde is null && mdi is null && cf is null)
+    Console.WriteLine("--- Calcul des intérets ---");
+    Console.Write("\nEntrer le capital (en Euros) : ");
+    string str_c = Console.ReadLine();
+    bool existCapital = double.TryParse(str_c, out c);
+    Console.Write("\nEntrer le taux interêt (en %) : ");
+    string str_tdi = Console.ReadLine();
+    bool existTaux_interet = double.TryParse(str_tdi, out tdi);
+    Console.Write("\nEntrer la durée epargne (en Année(s)) : ");
+    string str_dde = Console.ReadLine();
+    bool existEpargne = double.TryParse(str_dde, out dde);
+
+    double mi = c * (tdi / 100) * dde;
+    Console.WriteLine("\nLe montant des intérêts est de " + mi + " euros sur " + dde + " ans.");
+    cf = mi + c;
+    Console.WriteLine("\nVotre somme de capital après " + dde + " ans est de " + cf + " euros.");
+}*/
+#endregion
+
+#region "Exo4"
+/**
+int initNbTable = 1;
+int initNbRows = 1;
+int nbTable = 10;
+int nbRows = 10;
+string titre = "Les tables de multiplication";
+string subTitle = "Table de ";
+static string outputTable(int nb1, int nb2)
+{
+    return nb1 + " X " + nb2 + " = " +(nb1*nb2);
+}
+
+Console.WriteLine($"--- {titre} ---\n");
+
+for(int i = initNbTable; i <= nbTable; i++)
+{
+    Console.WriteLine(subTitle + i + " :");
+    for(int j = initNbRows; j <= nbRows; j++)
+    {
+        Console.WriteLine("        - " + outputTable(i,j));
+    }
+    Console.WriteLine("");
+}*/
 #endregion
