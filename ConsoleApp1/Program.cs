59a9e4a12b128fdd4f8460eb09b6fcb1a33ed2da
Angel Daniel PASTOR ROJAS <angeldaniel.pastorrojas@gmail.com>
2024-07-07 10:17:26 +0200

    feat: ajout de Exo4 ter

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -275,6 +275,20 @@ Console.WriteLine($"Il faudra 25 ans, nous serons en {2015 + nbAnnee}");
 Console.WriteLine($"Il y aura {Math.Floor(habitants)} habitants en {2015 + nbAnnee}");*/
 #endregion
 
+#region "Exo4 ter"
+/**
+Console.WriteLine("--- Menus et sous-menus ---\n");
+Console.WriteLine("table des matières :\n");
+for (int i = 1; i < 3 + 1; i++)
+{
+    Console.WriteLine($"\tChapitre {i}");
+    for (int j = 1; j < 3 + 1; j++)
+    {
+        Console.WriteLine($"\t\t- Partie {i}.{j}");
+    }
+}*/
+#endregion
+
 #region "Exo5"
 /** Objectif : Comprendre les fonctionnalités des structures conditionnelles For avec des variables int et double.
  * 
